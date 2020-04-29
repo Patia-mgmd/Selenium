8fae0fb69348ce4bea3faa8995dfcdd9cc28de55
patiaMgmd <54891217+patiaMgmd@users.noreply.github.com>
2020-04-29 20:06:44 +0300

    Update Search_test_1.java

--- Search_test_1.java
+++ Search_test_1.java
@@ -12,7 +12,6 @@ public class Search_test_1 {
     public static void main(String[] args) {
         //System.setProperty("webdriver.chrome.driver","C:\\Selenium\\Selenium\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         driver.get("https://habr.com/ru/");
@@ -21,8 +20,11 @@ public class Search_test_1 {
         WebElement input = driver.findElement(By.id("search-form-field"));
         input.sendKeys("Selenium");
         input.submit();
-        WebElement searchResult = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/section/div[1]/div[1]/div/a[1]/h3/span"));
-        System.out.println("Результат поиска:" + searchResult.getText());
+        
+        WebElement div = driver.findElement(By.className("page__body"));
+        List<WebElement> links = div.findElements(By.className("post__title"));
+
+        System.out.println("Результат поиска:" + links.size());
 
         driver.quit();
 
